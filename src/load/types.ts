34a5db378b6170ed91509db414e6de23536de0f5
Nick Amoscato <nick@amoscato.com>
2021-03-06 15:20:08 -0500

    Update dependabot config

--- src/load/types.ts
+++ src/load/types.ts
@@ -3,8 +3,5 @@ import {HasuraMetadataV2} from '@hasura/metadata'
 export type MetadataProperty = keyof HasuraMetadataV2
 
 export interface MetadataLoader {
-  load(
-    projectDir: string,
-    properties: MetadataProperty[]
-  ): Promise<HasuraMetadataV2>
+  load(projectDir: string): Promise<HasuraMetadataV2>
 }
